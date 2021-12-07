dc23d1c3336372881984e81dadaaa630bb54807e
elvdb <94414830+elvdb@users.noreply.github.com>
2021-12-07 12:56:46 -0500

    Added link to login page

--- sendIndividualReminder.php
+++ sendIndividualReminder.php
@@ -4,7 +4,7 @@
 // Admin clicks on "email" button
 // not sure how to retrieve email infor 
 // for particular professor
-include('config.php');
+include_once('db_connection.php');
 
     if (isset($_GET['email'])) {  
 
@@ -12,11 +12,11 @@ include('config.php');
 
         $to = $_GET["email"];
         $subject = 'Book list request';
-        $message = 'Hi, please submit your book request';
-        $header = "From: dbprojectfall21@gmail.com\r\nReply-To: dbprojectfall21@gmail.com";
+        $message = 'Hi, please submit your book request. Click on this link http://localhost/login.php to complete your submission.';
+        $header = "From: elilovera06@gmail.com\r\nReply-To: dbprojectfall21@gmail.com";
         $mail_sent = mail($to, $subject, $message, $header);
 
-        mail($to, $subject, $message, $headers);
+        mail($to, $subject, $message, $header);
 
         // TODO: redirect back to the page that shows the admin all the other users
     }
